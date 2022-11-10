18245cdf2ed2aaaa84bc6b03d299eefd52dd3f32
choihaneol <chu1004a@naver.com>
2022-11-10 00:36:47 -0500

    Customer Logging

---
+++ MagicVilla_VillaAPI/Logging/Logging.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MagicVilla_VillaAPI.Logging
+{
+    public class Logging : ILogging
+    {
+        public void Log(String message, string type)
+        {
+            if(type == "error")
+            {
+                Console.WriteLine("ERROR - " + message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+        }
+         
+    }
+}
+
--- MagicVilla_VillaAPI/Properties/Program.cs
+++ MagicVilla_VillaAPI/Properties/Program.cs
@@ -1,12 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿//using Serilog;
+
+
+using MagicVilla_VillaAPI.Logging;
+
+var builder = WebApplication.CreateBuilder(args); //Logging
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//Stop the default logger for customizing Logger (dependency injection)
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+//    .WriteTo.File("log/villaLogs.txt", rollingInterval:RollingInterval.Day).CreateLogger(); //generating log file every day
+//builder.Host.UseSerilog();
+
+builder.Services.AddControllers(option => {
+    //option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters(); //return type:json,xml
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<ILogging, LoggingV2>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
